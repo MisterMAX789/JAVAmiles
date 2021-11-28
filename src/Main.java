65cf81007642ce3de9db61c27d5f1b40a8d53a3c
MisterMAX789 <93163193+MisterMAX789@users.noreply.github.com>
2021-11-28 18:36:54 +0300

    miles commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost  = 13000;
+        int bonus = 20;
+        int miles = cost / bonus;
+        System.out.println(miles);
+    }
+}
